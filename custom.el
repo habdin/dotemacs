f4d1c6e7476e3176716c14ec4046b5e8878dfc14
Hassan Abdin <habdin@gmail.com>
2018-05-19 16:51:20 +0200

    Complete re-write for config.org

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(column-number-mode t)
  '(compilation-message-face (quote default))
  '(cua-global-mark-cursor-color "#2aa198")
@@ -11,13 +13,15 @@
  '(cua-read-only-cursor-color "#859900")
  '(custom-safe-themes
    (quote
-    ("3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" "01e067188b0b53325fc0a1c6e06643d7e52bc16b6653de2926a480861ad5aa78" "19ba41b6dc0b5dd34e1b8628ad7ae47deb19f968fe8c31853d64ea8c4df252b8" "551596f9165514c617c99ad6ce13196d6e7caa7035cea92a0e143dbe7b28be0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e3fc83cdb5f9db0d0df205f5da89af76feda8c56d79a653a5d092c82c7447e02" "2b8dff32b9018d88e24044eb60d8f3829bd6bbeab754e70799b78593af1c3aba" "b181ea0cc32303da7f9227361bb051bbb6c3105bb4f386ca22a06db319b08882" default)))
+    ("003a9aa9e4acb50001a006cfde61a6c3012d373c4763b48ceb9d523ceba66829" "0cd56f8cd78d12fc6ead32915e1c4963ba2039890700458c13e12038ec40f6f5" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" "01e067188b0b53325fc0a1c6e06643d7e52bc16b6653de2926a480861ad5aa78" "19ba41b6dc0b5dd34e1b8628ad7ae47deb19f968fe8c31853d64ea8c4df252b8" "551596f9165514c617c99ad6ce13196d6e7caa7035cea92a0e143dbe7b28be0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e3fc83cdb5f9db0d0df205f5da89af76feda8c56d79a653a5d092c82c7447e02" "2b8dff32b9018d88e24044eb60d8f3829bd6bbeab754e70799b78593af1c3aba" "b181ea0cc32303da7f9227361bb051bbb6c3105bb4f386ca22a06db319b08882" default)))
  '(display-battery-mode nil)
  '(display-time-mode nil)
  '(electric-indent-mode nil)
  '(electric-pair-mode t)
  '(fci-rule-color "#073642")
+ '(global-auto-revert-mode t)
  '(global-linum-mode t)
+ '(global-prettify-symbols-mode t)
  '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
  '(highlight-symbol-colors
    (--map
@@ -48,7 +52,7 @@
     ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
  '(package-selected-packages
    (quote
-    (dash dash-functional php-eldoc php-extras php-refactor-mode php-runtime php-scratch company-php treemacs-projectile treemacs neotree counsel-projectile counsel-pydoc yaml-mode projectile impatient-mode paradox underwater-theme undo-tree slime slime-company slime-repl abyss-theme color-theme color-theme-cobalt yasnippet-snippets web-beautify all-the-icons-gnus company-web rainbow-blocks rainbow-identifiers rainbow-mode rainbow-delimiters web-mode ox-pandoc helm-dash minesweeper lorem-ipsum php-mode erc-colorize dired-imenu all-the-icons-dired all-the-icons-ivy mode-icons magit-gh-pulls magit-gitflow magit-imerge magit magithub intellij-theme multiple-cursors async org-magit ivy-hydra labburn-theme airline-themes use-package smex company-auctex company-anaconda counsel ob-ipython htmlize auto-package-update powerline emmet-mode ## org sublimity solarized-theme py-autopep8 flycheck elpy ein better-defaults)))
+    (php-refractor-mode page-break-lines session volatile-highlights diminish multipe-cursors company-mode dash dash-functional php-eldoc php-extras php-refactor-mode php-runtime php-scratch company-php neotree counsel-projectile counsel-pydoc yaml-mode projectile impatient-mode paradox underwater-theme undo-tree slime slime-company slime-repl abyss-theme color-theme color-theme-cobalt yasnippet-snippets web-beautify all-the-icons-gnus company-web rainbow-blocks rainbow-identifiers rainbow-mode rainbow-delimiters web-mode ox-pandoc helm-dash minesweeper lorem-ipsum php-mode erc-colorize dired-imenu all-the-icons-dired all-the-icons-ivy mode-icons magit-gh-pulls magit-gitflow magit-imerge magit magithub intellij-theme multiple-cursors async org-magit ivy-hydra labburn-theme airline-themes use-package company-auctex company-anaconda counsel ob-ipython htmlize auto-package-update powerline emmet-mode ## org solarized-theme py-autopep8 flycheck elpy ein better-defaults)))
  '(paradox-automatically-star nil)
  '(pos-tip-background-color "#073642")
  '(pos-tip-foreground-color "#93a1a1")
@@ -56,6 +60,8 @@
  '(rainbow-identifiers-cie-l*a*b*-color-count 1024 t)
  '(rainbow-identifiers-cie-l*a*b*-lightness 80 t)
  '(rainbow-identifiers-cie-l*a*b*-saturation 25 t)
+ '(recentf-mode t)
+ '(session-use-package t nil (session))
  '(size-indication-mode nil)
  '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
  '(sublimity-mode nil)
@@ -90,7 +96,8 @@
  '(xterm-color-names
    ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
  '(xterm-color-names-bright
-   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
+   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"])
+ '(yas-global-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
